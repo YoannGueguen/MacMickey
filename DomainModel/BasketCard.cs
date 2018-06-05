91bd7092a11120b40962b1bfb48cfadba7dfc2de
Yoann <yoann-gueguen@hotmail.fr>
2018-06-05 21:10:08 +0200

    add panier

---
+++ DomainModel/BasketCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MacMickey.DomainModel
+{
+    public class BasketCard
+    {
+        [Key]
+        public string BasketCardId { get; set; }
+        public string CardId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
