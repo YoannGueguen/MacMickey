909374058514cef50b3b0b113db2dfeb0a426cc2
DESKTOP-SCL0V21\Nicolas Vienat <v.nico@hotmail.fr>
2018-06-06 00:33:42 +0200

    Ajout Data Persons

---
+++ DomainModel/Employee.cs
@@ -0,0 +1,21 @@
+﻿using MacMickey.DomainModel.Persons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MacMickey.DomainModel
+{
+    public class Employee : Person
+    {
+        public string OfficeName { get; set; }
+        public bool IsJunior { get; set; }
+
+        public virtual ICollection<Customer> ListCustomer { get; set; }
+        public virtual Manager Manager { get; set; }
+
+        /// <summary>
+        /// List of menu linked to the current beverage
+        /// </summary>
+        //public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
---
+++ DomainModel/Manager.cs
@@ -0,0 +1,20 @@
+﻿using MacMickey.DomainModel.Persons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MacMickey.DomainModel
+{
+    public class Manager : Person
+    {
+        public string OfficeName { get; set; }
+        public bool IsJunior { get; set; }
+
+        public virtual ICollection<Employee> ListEmployees { get; set; }
+
+        /// <summary>
+        /// List of menu linked to the current beverage
+        /// </summary>
+        //public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
